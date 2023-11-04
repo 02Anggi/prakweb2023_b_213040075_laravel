6463d412e69660c4ed31c9f7f4e9185b401264dc
Muhammad Anggi Kusuma <raden@DESKTOP-K1JPJT5.com>
2023-11-04 23:28:22 +0700

    Video 4 Blade Templating Engine

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Anggi Blog | Posts</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <h1>Halaman Posts</h1>
-</body>
-<script src="js/script.js"></script>
-</html>
+@extends('layouts.main')
+
+@section('container')
+
+    @foreach ($posts as $post)
+        <article class="mb-5">
+            <h2>
+                <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
+            </h2>
+            <h5>{{ $post["author"] }}</h5>
+            <p>{{ $post["body"] }}</p>
+        </article>
+    @endforeach
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Muhammad Anggi Kusuma",
         "email" => "anggi.213040075@mail.unpas.ac.id",
         "image" => "img1.png" 
@@ -26,5 +29,54 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Muhammad Anggi Kusuma",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non nulla veniam amet, in natus cumque, culpa beatae nihil, error vero quibusdam? Id quos maxime sequi perspiciatis corrupti libero blanditiis mollitia quas atque ipsam, sed illo eius optio numquam voluptatibus unde incidunt voluptatum sint quia voluptates voluptatem provident. Distinctio qui error accusantium sunt sint placeat soluta ipsa assumenda praesentium quam tenetur ducimus, eaque quia pariatur voluptas nesciunt amet mollitia odit natus? Sint possimus cumque voluptates blanditiis harum in, voluptatibus mollitia temporibus?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Muhammad Angga Kusuma",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque rem ipsa quibusdam, doloribus laborum dicta quos labore quaerat nihil voluptatibus soluta obcaecati, corrupti vel, deleniti totam quae commodi quod? Molestias explicabo, quisquam illum fuga corporis dolor excepturi dicta sapiente temporibus ab blanditiis accusamus dignissimos dolore, eos officia? Debitis culpa soluta, tempora sint neque cupiditate dolorum exercitationem porro maiores consequuntur consectetur suscipit provident fuga quia, nihil magni cumque ipsam, rerum officia! Expedita, deleniti! Ut fugit provident ipsum, et explicabo reprehenderit. Quasi pariatur cupiditate ab amet molestiae voluptas, incidunt deserunt corporis beatae minima nulla facilis eligendi adipisci officia a dolorum accusamus illo!"
+        ],
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// Halaman single post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Muhammad Anggi Kusuma",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non nulla veniam amet, in natus cumque, culpa beatae nihil, error vero quibusdam? Id quos maxime sequi perspiciatis corrupti libero blanditiis mollitia quas atque ipsam, sed illo eius optio numquam voluptatibus unde incidunt voluptatum sint quia voluptates voluptatem provident. Distinctio qui error accusantium sunt sint placeat soluta ipsa assumenda praesentium quam tenetur ducimus, eaque quia pariatur voluptas nesciunt amet mollitia odit natus? Sint possimus cumque voluptates blanditiis harum in, voluptatibus mollitia temporibus?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Muhammad Angga Kusuma",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque rem ipsa quibusdam, doloribus laborum dicta quos labore quaerat nihil voluptatibus soluta obcaecati, corrupti vel, deleniti totam quae commodi quod? Molestias explicabo, quisquam illum fuga corporis dolor excepturi dicta sapiente temporibus ab blanditiis accusamus dignissimos dolore, eos officia? Debitis culpa soluta, tempora sint neque cupiditate dolorum exercitationem porro maiores consequuntur consectetur suscipit provident fuga quia, nihil magni cumque ipsam, rerum officia! Expedita, deleniti! Ut fugit provident ipsum, et explicabo reprehenderit. Quasi pariatur cupiditate ab amet molestiae voluptas, incidunt deserunt corporis beatae minima nulla facilis eligendi adipisci officia a dolorum accusamus illo!"
+        ],
+    ];
+    
+    $new_post = [];
+
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
