286e494b533188efa16f125a835be9a8cf4f23e2
Muhammad Anggi Kusuma <raden@DESKTOP-K1JPJT5.com>
2023-11-04 21:29:35 +0700

    Video 3 Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Muhammad Anggi Kusuma",
+        "email" => "anggi.213040075@mail.unpas.ac.id",
+        "image" => "img1.png" 
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
