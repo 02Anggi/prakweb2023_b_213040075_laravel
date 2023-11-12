6ba2f49ae402168e96c6576b39c36745677afbc3
Muhammad Anggi Kusuma <raden@DESKTOP-K1JPJT5.com>
2023-11-12 23:00:59 +0700

    Video 9 Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,60 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Muhammad Anggi Kusuma',
+            'email' => 'anggi.213040075@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Muhammad Angga Kusuma',
+            'email' => 'angga.213040074@mail.unpas.ac.id',
+            'password' => bcrypt('54321')
+        ]);
+
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming',
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama', 
+            'slug' => 'judul-pertama', 
+            'excerpt' => 'Lorem ipsum pertama',
+            'body' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi, repellat iusto asperiores repellendus illo facilis provident recusandae at.</p> <p>Saepe mollitia rerum repellendus sint ducimus. Odit necessitatibus animi sint, dolore similique nam repellendus tempore aliquid, nobis, sed mollitia sunt ratione? Nesciunt aut, veniam dolorum laborum in minima saepe aliquam sed. </p> <p>Quibusdam libero excepturi voluptas corrupti ipsa hic doloremque, facilis eum odio. Quis porro deleniti atque ex consectetur! Voluptas sint ullam ab incidunt tenetur earum iste provident deleniti vel ea placeat, assumenda non. </p>', 
+            'category_id' => 1, 
+            'user_id' => 1]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua', 
+            'slug' => 'judul-ke-dua', 
+            'excerpt' => 'Lorem ipsum kedua',
+            'body' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi, repellat iusto asperiores repellendus illo facilis provident recusandae at.</p> <p>Saepe mollitia rerum repellendus sint ducimus. Odit necessitatibus animi sint, dolore similique nam repellendus tempore aliquid, nobis, sed mollitia sunt ratione? Nesciunt aut, veniam dolorum laborum in minima saepe aliquam sed. </p> <p>Quibusdam libero excepturi voluptas corrupti ipsa hic doloremque, facilis eum odio. Quis porro deleniti atque ex consectetur! Voluptas sint ullam ab incidunt tenetur earum iste provident deleniti vel ea placeat, assumenda non. </p>', 
+            'category_id' => 1, 
+            'user_id' => 1]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga', 
+            'slug' => 'judul-ke-tiga', 
+            'excerpt' => 'Lorem ipsum ketiga',
+            'body' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi, repellat iusto asperiores repellendus illo facilis provident recusandae at.</p> <p>Saepe mollitia rerum repellendus sint ducimus. Odit necessitatibus animi sint, dolore similique nam repellendus tempore aliquid, nobis, sed mollitia sunt ratione? Nesciunt aut, veniam dolorum laborum in minima saepe aliquam sed. </p> <p>Quibusdam libero excepturi voluptas corrupti ipsa hic doloremque, facilis eum odio. Quis porro deleniti atque ex consectetur! Voluptas sint ullam ab incidunt tenetur earum iste provident deleniti vel ea placeat, assumenda non. </p>',
+            'category_id' => 2, 
+            'user_id' => 1]);
+        
+        Post::create([
+            'title' => 'Judul Ke Tiga', 
+            'slug' => 'judul-ke-tiga', 
+            'excerpt' => 'Lorem ipsum ketiga',
+            'body' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi, repellat iusto asperiores repellendus illo facilis provident recusandae at.</p> <p>Saepe mollitia rerum repellendus sint ducimus. Odit necessitatibus animi sint, dolore similique nam repellendus tempore aliquid, nobis, sed mollitia sunt ratione? Nesciunt aut, veniam dolorum laborum in minima saepe aliquam sed. </p> <p>Quibusdam libero excepturi voluptas corrupti ipsa hic doloremque, facilis eum odio. Quis porro deleniti atque ex consectetur! Voluptas sint ullam ab incidunt tenetur earum iste provident deleniti vel ea placeat, assumenda non. </p>',
+            'category_id' => 2, 
+            'user_id' => 2]);    
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.main')
 
 @section('container')
-
     <article class="mb-5">
+
         <h2>{{ $post->title }}</h2>
 
-        <p>By. Muhammad Anggi Kusuma in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
         {!! $post->body !!}
 
     </article>
     
-    <a href="/blog">Back to Posts</a>
+    <a href="/blog" class="d-block mt-3">Back to Posts</a>
 @endsection
